4737a29278a811f97d6b4171ea51d989fba8eca3
Qazxm <seyeong025@gmail.com>
2024-07-11 23:42:29 +0900

    QQ

---
+++ app/src/main/java/com/example/sql/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.sql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MainActivity extends AppCompatActivity {
+
+    Connection connect;
+    String ConnectionResult="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void GetTextfromSQL(View v)
+    {
+        TextView tx1 =(TextView) findViewById(R.id.textView);
+        TextView tx2 =(TextView) findViewById(R.id.textView2);
+
+        try{
+            ConnectionHelper connectionHelper=new ConnectionHelper();
+            connect = connectionHelper.connectionclass();
+            if(connect!=null)
+            {
+                String query="Select *from Users";
+                Statement st=connect.createStatement();
+                ResultSet rs=st.executeQuery(query);
+
+                while (rs.next())
+                {
+                    tx1.setText(rs.getString(1));
+                    tx2.setText(rs.getString(2));
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+
+        }
+    }
+}
